cd6ed1717620a4dd23aaea0f65c8332f172ca087
MaxDumntl <maxime.pascal.dumonteil@gmail.com>
2021-11-30 23:18:25 +0100

    Creation des premieres classes

---
+++ JolieCite_projetApOb_ddd/src/domain/Artist.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class Artist {
+
+    String name;
+}
---
+++ JolieCite_projetApOb_ddd/src/domain/Salle.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Salle {
+    /*
+    capacite
+    horaires
+    date ouverture (1 par jour)
+    list d'evenements programmes
+     */
+}
