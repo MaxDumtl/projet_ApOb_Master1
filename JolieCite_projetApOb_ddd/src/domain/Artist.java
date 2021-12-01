9c54bdb960e5d85e03dd39e73bdc2bb9dab028e1
MaxDumtl <maxime.pascal.dumonteil@gmail.com>
2021-12-01 15:43:05 +0100

    Implementation de la classe Artist (domain) + completion du gitignore

--- JolieCite_projetApOb_ddd/src/domain/Artist.java
+++ JolieCite_projetApOb_ddd/src/domain/Artist.java
@@ -1,6 +1,25 @@
 package domain;
 
-public class Artist {
+import java.util.Objects;
 
-    String name;
+public class Artist { // Value Object
+
+    private final String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artist artist = (Artist) o;
+        return Objects.equals(name, artist.name);
+    }
+
 }
