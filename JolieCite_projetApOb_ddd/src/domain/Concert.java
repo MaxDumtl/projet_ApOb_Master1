4d32b522de04425e4d68d980f05e39c69848a726
MaxDumtl <maxime.pascal.dumonteil@gmail.com>
2021-12-01 15:48:56 +0100

    Implementation des attributs de Concert (domain)

--- JolieCite_projetApOb_ddd/src/domain/Concert.java
+++ JolieCite_projetApOb_ddd/src/domain/Concert.java
@@ -1,4 +1,36 @@
 package domain;
 
-public class Concert {
+import java.util.Date;
+
+public class Concert { //Entity
+    private static int globalId = 0;
+    private final int id; //TODO check if necessary to encapsule ConcertId in a ValueObject if it's another thing than an int
+
+    private Date date;
+    private Artist artist;
+
+    public Concert() { //TODO this constructor is necessary ?
+        //initialize id
+        id = globalId++;
+    }
+
+    public Concert(Date date, Artist artist) {
+        //initialize id
+        id = globalId++;
+
+        this.date = date;
+        this.artist = artist;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
 }
